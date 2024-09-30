b8e9a49c9bf1f78d101248a44bc474cc6d9746fb
Jon <jon.martin.sigvaldsen@gmail.com>
2024-09-30 09:24:04 +0200

    Re-added friendly snippets, with an exclude

--- init.lua
+++ init.lua
@@ -427,7 +427,9 @@ mason_lspconfig.setup_handlers {
 -- [[ Configure nvim-cmp ]]
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load {
+  exclude = { "cpp" },
+}
 luasnip.config.setup {}
 
 cmp.setup {
