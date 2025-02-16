66765daa81ef9b5ae0312f727c1f31364ebd087b
jon <jon@mail.com>
2025-02-16 23:10:05 +0100

    Tweaks to keybinds, README-rewritten

--- init.lua
+++ init.lua
@@ -5,6 +5,26 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Nowrap
+vim.opt.wrap = false
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- Enable mouse mode
+vim.o.mouse = 'i'
+
 require('keybinds')
 require('autocmds')
 require('functions')
