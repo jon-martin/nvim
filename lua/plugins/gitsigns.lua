1432e1f9510dc67a4376908cfe7f03aaa073c7d9
Jon Martin <jon.martin.sigvaldsen@gmail.com>
2026-08-08 00:34:55 +0200

    keybind additions and changes

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,36 @@
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', '[G', function()
+        gitsigns.nav_hunk('first')
+    end)
+
+    map('n', '[g', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    map('n', ']g', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', ']G', function()
+        gitsigns.nav_hunk('last')
+    end)
+  end
+}
